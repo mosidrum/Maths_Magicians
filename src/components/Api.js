677694ef2a1d17e2e073c1cff4420602bf006366
mosidrum <mosiokanga@gmail.com>
2023-06-01 09:40:39 +0100

    Fetch from API

---
+++ src/components/Api.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+function Api(link) {
+  const [res, setRes] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [author, setAuthor] = useState(null);
+  const [error, setError] = useState(null);
+  const key = 'd28j3TKK1RJuSx6FptfY5A==RchWuPY71oCqNThz';
+  useEffect(() => {
+    if (res === null) {
+      setLoading(true);
+      fetch(link, {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': key,
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          // setRes(result);
+          setRes(result[0].quote);
+          setAuthor(result[0].author);
+        })
+        .catch((error) => {
+          setError(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [res, link, key]);
+  return {
+    res, loading, error, author,
+  };
+}
+
+export default Api;
--- src/components/pages/Calculator.js
+++ src/components/pages/Calculator.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-import Screen from './Screen';
+import Screen from '../Screen';
 import './calculator.css';
 import calculate from '../../logic/calculate';
+import Api from '../Api';
+import Loading from '../../assets/loading.gif';
 
 const Calculator = () => {
   const [state, setState] = useState({ total: 0, next: null, operation: null });
@@ -9,69 +11,84 @@ const Calculator = () => {
     setState(() => calculate(state, e.target.innerText));
   };
   const { total, operation, next } = state;
+  const {
+    res, loading, error, author,
+  } = Api('https://api.api-ninjas.com/v1/quotes?category=computers');
+  if (loading) {
+    return <img src={Loading} className="loader" alt="Loader" />;
+  }
+  if (error) {
+    return <p>There was an error fetching the quote</p>;
+  }
   return (
-    <section className="my-calculator">
-      <Screen total={total} operation={operation} next={next} />
-      <div className="buttons">
-        <button type="button" className="ac" onClick={solve}>
-          AC
-        </button>
-        <button type="button" className="plmi" onClick={solve}>
-          +/-
-        </button>
-        <button type="button" className="percent" onClick={solve}>
-          %
-        </button>
-        <button type="button" className="division orange" onClick={solve}>
-          &#247;
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          7
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          8
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          9
-        </button>
-        <button type="button" className="multiplication orange" onClick={solve}>
-          x
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          4
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          5
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          6
-        </button>
-        <button type="button" className="subtract orange" onClick={solve}>
-          -
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          1
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          2
-        </button>
-        <button type="button" className="digit" onClick={solve}>
-          3
-        </button>
-        <button type="button" className="addition orange" onClick={solve}>
-          +
-        </button>
-        <button type="button" className="zero" onClick={solve}>
-          0
-        </button>
-        <button type="button" className="decimal" onClick={solve}>
-          .
-        </button>
-        <button type="button" className="equal orange" onClick={solve}>
-          =
-        </button>
+    <div className="container">
+      <div className="quote">
+        <h2>{author}</h2>
+        <p>{res}</p>
       </div>
-    </section>
+      <section className="my-calculator">
+        <Screen total={total} operation={operation} next={next} />
+        <div className="buttons">
+          <button type="button" className="ac" onClick={solve}>
+            AC
+          </button>
+          <button type="button" className="plmi" onClick={solve}>
+            +/-
+          </button>
+          <button type="button" className="percent" onClick={solve}>
+            %
+          </button>
+          <button type="button" className="division orange" onClick={solve}>
+            &#247;
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            7
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            8
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            9
+          </button>
+          <button type="button" className="multiplication orange" onClick={solve}>
+            x
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            4
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            5
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            6
+          </button>
+          <button type="button" className="subtract orange" onClick={solve}>
+            -
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            1
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            2
+          </button>
+          <button type="button" className="digit" onClick={solve}>
+            3
+          </button>
+          <button type="button" className="addition orange" onClick={solve}>
+            +
+          </button>
+          <button type="button" className="zero" onClick={solve}>
+            0
+          </button>
+          <button type="button" className="decimal" onClick={solve}>
+            .
+          </button>
+          <button type="button" className="equal orange" onClick={solve}>
+            =
+          </button>
+        </div>
+      </section>
+    </div>
   );
 };
 
